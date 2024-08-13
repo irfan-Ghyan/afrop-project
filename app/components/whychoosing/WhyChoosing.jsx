a50df7054ca78622aa84a3bb9d1a3a2b4cb21c67
irfan-Ghyan <irfan@teleios.ae>
2024-08-13 11:29:47 +0400

    done text

--- app/components/whychoosing/WhyChoosing.jsx
+++ app/components/whychoosing/WhyChoosing.jsx
@@ -1,22 +1,9 @@
 import React from 'react';
-import afro from '../../../public/images/arfo.png';
-import Image from 'next/image';
-import Link from 'next/link';
 
-const Aboutus = ({ title, description, buttonText, imageSrc, imageAlt, heading, paragraph, paragraph1, paragraph2 }) => {
+
+const WhyChoosing = ({ heading, paragraph, paragraph1, paragraph2 }) => {
   return (
     <div className='container mx-auto px-4 py-6'>
-        <div className='flex justify-center items-center my-[60px]'>
-            <Image src={afro} alt='bannar' width={600} height={350}/>
-        </div>
-        <div className="gap-8 justify-between">
-        <div className="w-full  flex flex-col bg-transparent p-6 items-center">
-          <h2 className="font-black text-4xl lg:text-6xl text-[#ad8c19] mb-4">{title}</h2>
-          <p className="text-gray-700 text-4xl font-thin mb-4 text-balance">{description}</p>
-          <Link href="/about" className="w-[200px] my-[20px] border-2 border-[#ad8c19] text-gray-700 text-[14px] text-center py-4 px-4 rounded-full font-normal hover:bg-gradient-to-r hover:from-[#2b5c17] hover:to-[#ad8c19] hover:text-white">
-            {buttonText}
-          </Link>
-        </div>
         <div className="w-full bg-black bg-transparent p-8" style={{ backgroundImage: "url('/images/slide.jpg')"  }} >
           <h2 className="font-bold text-2xl lg:text-4xl text-white mb-4">{heading}</h2>
           <div className='items-center text-center flex justify-between py-10'>
@@ -34,9 +21,8 @@ const Aboutus = ({ title, description, buttonText, imageSrc, imageAlt, heading, 
           </div>
           </div>
         </div>
-      </div>
     </div>
   )
 }
 
-export default Aboutus;
+export default WhyChoosing;
--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ import Card from './components/card/Card';
 import ViewProducts from './components/viewproducts/ViewProducts';
 import CompaniesSlider from "./components/compinesslider/CompaniesSlider";
 import ContactForm from "./components/contactform/ContactForm";
+import WhyChoosing from "./components/whychoosing/WhyChoosing";
 
 export default function Home() {
   const data = {
@@ -15,8 +16,9 @@ export default function Home() {
     imageSrc: '/images/ceo.png',
     imageAlt: 'Example Image',
     heading: 'WHY CHOOSING AFROHARVEST?',
-    paragraph:'Welcome to Afroharvest, where excellence meets nourishment. As a leading enterprise in the heart of Sudan, we take immense pride in our role as specialists in the export of high-quality Sudanese origin commodities and crops. Our commitment to quality, sustainability, and innovation sets us apart in the agricultural and food production industry',};
-
+    paragraph:'We are committed to delivering top-notch products by adhering to rigous quality control processes. Our focus on quality ensures that evry product meets international standards.',
+    paragraph1:'Our effiecient logistics and supply chain management gurantee timely and reliable delivery, minimizing and potential disryptions.',
+    paragraph2:'Our experienced team brings expertise and dedication to every aspect of our operations, ensuring that our clients receive exceptional service.'};
   const items = [
     '/images/img4.jpg',
     '/images/img1.jpg',
@@ -32,6 +34,7 @@ export default function Home() {
     <div>
     {/* <BusinessCategory /> */}
     <Aboutus {...data}/>
+    <WhyChoosing />
     <FeatureComponent />
     <div className="mt-20"><h1 className="text-center text-4xl font-black">OUR PRODUCTS</h1>
     <Card />
